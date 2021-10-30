ef343bf6c08eb9172cba1622bbe0353daf5af680
Florian <florian@mail.fr>
2021-10-30 12:40:42 +0200

    'ScrollTrigger' sur les sections Chef/Réservation

--- animation.js
+++ animation.js
@@ -6,19 +6,30 @@ const header = document.querySelector('header');
 
 /* SECTION PRESENTATION */
 const imgLeft = document.getElementById('card1');
-const imgRight = document.getElementById('card2');
 const txtLeft = document.getElementById('txt1');
+const imgRight = document.getElementById('card2');
 const txtRight = document.getElementById('txt2');
 
+/* MENU */
+const menu = Array.from(document.getElementById('menu').children);
+const navigation = document.querySelector('section.navigation');
 
-const TL = gsap.timeline({
+/* CHEF */
+const imgChef = document.getElementById('imgChef');
+const txtChef = document.querySelector('section.chief .block-body');
+
+/* RESERVATION */
+const txtReservation = Array.from(document.getElementById('bodyReservation').children)
+
+
+/* ANIMATION HEADER */
+const TLHeader = gsap.timeline({
     defaults: {
         duration: 0.4
     }
 });
 
-/* ANIMATION HEADER */
-TL
+TLHeader
 .from(header, {
     backgroundPositionY: '-80vh',
     duration: 0.7,
@@ -40,7 +51,7 @@ TL
 })
 
 
-/* SECTION PRESENTATION */
+/* ANIMATION SECTION PRESENTATION */
 gsap.from([imgLeft, txtLeft, txtLeft.children], {
     x: 100,
     opacity: 0,
@@ -48,7 +59,7 @@ gsap.from([imgLeft, txtLeft, txtLeft.children], {
     stagger: 0.2,
     scrollTrigger: {
         trigger: '.presentation .block-left',
-        start: "top-=200 center",
+        start: "top-=100 center",
         end: "bottom center",
         toggleActions: "play reverse play none",
     }
@@ -60,9 +71,67 @@ gsap.from([imgRight, txtRight, txtRight.children], {
     stagger: 0.2,
     scrollTrigger: {
         trigger: '.presentation .block-right',
-        markers: true,
-        start: "top-=200 center",
+        start: "top-=100 center",
         end: "bottom center",
         toggleActions: "play reverse play none",
     }
 })
+
+/* ANIMATION MENU ET NAVIGATION */
+const fade = {
+    x: 100,
+    opacity: 0,
+    duration: 1,
+    stagger: 0.2
+}
+gsap.from(menu, {
+    ...fade,
+    scrollTrigger: {
+        trigger: 'section.navigation',
+        start: "top-=300 center",
+        end: "bottom+=200 center",
+        toggleActions: "play reverse play none",
+    }
+});
+gsap.from(['section.navigation nav a'], {
+    ...fade,
+    scrollTrigger: {
+        trigger: 'section.navigation',
+        start: "top-=300 center",
+        end: "bottom+=200 center",
+        toggleActions: "play reverse play none",
+    }
+});
+gsap.from(['section.navigation'], {
+    ...fade,
+    scrollTrigger: {
+        trigger: 'section.navigation',
+        start: "top-=300 center",
+        end: "bottom+=200 center",
+        toggleActions: "play reverse play none",
+    }
+});
+
+
+/* ANIMATION SECTION CHEF */
+gsap.from([imgChef, txtChef.children], {
+    ...fade,
+    scrollTrigger: {
+        trigger: 'section.chief',
+        start: "top-=100 center",
+        end: "bottom+=100 center",
+        toggleActions: "play reverse play none",
+    }
+});
+
+
+/* ANIMATION RESERVATION */
+gsap.from(txtReservation, {
+    ...fade,
+    scrollTrigger: {
+        trigger: 'section.reservation',
+        start: "top-=100 center",
+        end: "bottom+=100 center",
+        toggleActions: "play reverse play none",
+    }
+});
