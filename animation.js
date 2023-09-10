8f75eaec23c6e179f2bda0cf08e2c82dfcfa30b9
Florian <sicilia.florian02@gmail.com>
2023-09-10 14:58:03 +0200

    Suppression des animations de retours

--- animation.js
+++ animation.js
@@ -1,138 +1,125 @@
 /* HEADER */
-const title = Array.from(document.querySelectorAll('.container-slogan > *'));
-const logo = document.querySelector('.logo');
-const links = Array.from(document.querySelectorAll('nav.menu a'));
-const header = document.querySelector('header');
+const title = Array.from(document.querySelectorAll(".container-slogan > *"));
+const logo = document.querySelector(".logo");
+const links = Array.from(document.querySelectorAll("nav.menu a"));
+const header = document.querySelector("header");
 
 /* SECTION PRESENTATION */
-const imgLeft = document.getElementById('card1');
-const txtLeft = document.getElementById('txt1');
-const imgRight = document.getElementById('card2');
-const txtRight = document.getElementById('txt2');
+const imgLeft = document.getElementById("card1");
+const txtLeft = document.getElementById("txt1");
+const imgRight = document.getElementById("card2");
+const txtRight = document.getElementById("txt2");
 
 /* MENU */
-const menu = Array.from(document.getElementById('menu').children);
-const navigation = document.querySelector('section.navigation');
+const menu = Array.from(document.getElementById("menu").children);
+const navigation = document.querySelector("section.navigation");
 
 /* CHEF */
-const imgChef = document.getElementById('imgChef');
-const txtChef = document.querySelector('section.chief .block-body');
+const imgChef = document.getElementById("imgChef");
+const txtChef = document.querySelector("section.chief .block-body");
 
 /* RESERVATION */
-const txtReservation = Array.from(document.getElementById('bodyReservation').children)
-
+const txtReservation = Array.from(document.getElementById("bodyReservation").children);
 
 /* ANIMATION HEADER */
 const TLHeader = gsap.timeline({
-    defaults: {
-        duration: 0.4
-    }
+	defaults: {
+		duration: 0.4,
+	},
 });
 
-TLHeader
-.from(header, {
-    backgroundPositionY: '-80vh',
-    duration: 0.7,
-    ease: "circ.out"
+TLHeader.from(header, {
+	backgroundPositionY: "-80vh",
+	duration: 0.7,
+	ease: "circ.out",
 })
-.from(title, {
-    x: 100,
-    opacity: 0,
-    stagger: 0.2
-})
-.from(logo, {
-    x: 50,
-    opacity: 0,
-})
-.from(links, {
-    x: 50,
-    opacity: 0,
-    stagger: 0.1
-})
-
+	.from(title, {
+		x: 100,
+		opacity: 0,
+		stagger: 0.2,
+	})
+	.from(logo, {
+		x: 50,
+		opacity: 0,
+	})
+	.from(links, {
+		x: 50,
+		opacity: 0,
+		stagger: 0.1,
+	});
 
 /* ANIMATION SECTION PRESENTATION */
 gsap.from([imgLeft, txtLeft, txtLeft.children], {
-    x: 100,
-    opacity: 0,
-    duration: 0.6,
-    stagger: 0.2,
-    scrollTrigger: {
-        trigger: '.presentation .block-left',
-        start: "top-=100 center",
-        end: "bottom center",
-        toggleActions: "play reverse play none",
-    }
-})
+	x: 100,
+	opacity: 0,
+	duration: 0.6,
+	stagger: 0.2,
+	scrollTrigger: {
+		trigger: ".presentation .block-left",
+		start: "top-=100 center",
+		end: "bottom center",
+	},
+});
 gsap.from([imgRight, txtRight, txtRight.children], {
-    x: -100,
-    opacity: 0,
-    duration: 0.6,
-    stagger: 0.2,
-    scrollTrigger: {
-        trigger: '.presentation .block-right',
-        start: "top-=100 center",
-        end: "bottom center",
-        toggleActions: "play reverse play none",
-    }
-})
+	x: -100,
+	opacity: 0,
+	duration: 0.6,
+	stagger: 0.2,
+	scrollTrigger: {
+		trigger: ".presentation .block-right",
+		start: "top-=100 center",
+		end: "bottom center",
+	},
+});
 
 /* ANIMATION MENU ET NAVIGATION */
 const fade = {
-    x: 100,
-    opacity: 0,
-    duration: 1,
-    stagger: 0.2
-}
+	x: 100,
+	opacity: 0,
+	duration: 1,
+	stagger: 0.2,
+};
 gsap.from(menu, {
-    ...fade,
-    scrollTrigger: {
-        trigger: 'section.navigation',
-        start: "top-=300 center",
-        end: "bottom+=200 center",
-        // markers: true,
-        toggleActions: "play reverse play none",
-    }
+	...fade,
+	scrollTrigger: {
+		trigger: "section.navigation",
+		start: "top-=300 center",
+		end: "bottom+=200 center",
+	},
 });
-gsap.from(['section.navigation nav a'], {
-    ...fade,
-    scrollTrigger: {
-        trigger: 'section.navigation',
-        start: "top-=100 center",
-        end: "bottom+=200 center",
-        toggleActions: "play reverse play none",
-    }
+gsap.from(["section.navigation nav a"], {
+	...fade,
+	scrollTrigger: {
+		trigger: "section.navigation",
+		start: "top-=100 center",
+		end: "bottom+=200 center",
+	},
 });
-gsap.from(['section.navigation'], {
-    ...fade,
-    scrollTrigger: {
-        trigger: 'section.navigation',
-        start: "top-=300 center",
-        end: "bottom+=200 center",
-        toggleActions: "play reverse play none",
-    }
+gsap.from(["section.navigation"], {
+	...fade,
+	scrollTrigger: {
+		trigger: "section.navigation",
+		start: "top-=300 center",
+		end: "bottom+=200 center",
+	},
 });
-
 
 /* ANIMATION SECTION CHEF */
 gsap.from([imgChef, txtChef.children], {
-    ...fade,
-    scrollTrigger: {
-        trigger: 'section.chief',
-        start: "top-=100 center",
-        end: "bottom+=100 center",
-        toggleActions: "play reverse play none",
-    }
+	...fade,
+	scrollTrigger: {
+		trigger: "section.chief",
+		start: "top-=100 center",
+		end: "bottom+=100 center",
+	},
 });
-
 
 /* ANIMATION RESERVATION */
 gsap.from(txtReservation, {
-    ...fade,
-    scrollTrigger: {
-        trigger: 'section.reservation',
-        start: "top-=100 center",
-        end: "bottom+=100 center",
-        toggleActions: "play reverse play none",
-    }
+	...fade,
+	scrollTrigger: {
+		trigger: "section.reservation",
+		start: "top-=100 center",
+		end: "bottom+=100 center",
+	},
 });
--- script.js
+++ script.js
@@ -1,67 +1,55 @@
-const html = document.querySelector('html');
-const btn = document.querySelector('.hamburger');
-const nav = document.querySelector('.menu');
-let touchStart = touchEnd = 0;
+const html = document.querySelector("html");
+const btn = document.querySelector(".hamburger");
+const nav = document.querySelector(".menu");
+let touchStart = (touchEnd = 0);
 
-const ongletsLinks = Array.from(document.querySelectorAll('.link-nav'));
-const contenu = document.querySelectorAll('.container-menu');
+const ongletsLinks = Array.from(document.querySelectorAll(".link-nav"));
+const contenu = document.querySelectorAll(".container-menu");
 
 // Menu Mobile / Scroll
-btn.addEventListener('click', () =>
-{
-    btn.classList.toggle('active');
-    nav.classList.toggle('active');
+btn.addEventListener("click", () => {
+	btn.classList.toggle("active");
+	nav.classList.toggle("active");
 
-    if(nav.classList.contains('active'))
-        html.classList.add('no-scroll');
-    else
-        html.classList.remove('no-scroll');
+	if (nav.classList.contains("active")) html.classList.add("no-scroll");
+	else html.classList.remove("no-scroll");
 });
 
-nav.addEventListener('touchstart', (e) => touchStart = e.targetTouches[0].clientX);
+nav.addEventListener("touchstart", (e) => (touchStart = e.targetTouches[0].clientX));
 
-nav.addEventListener('touchmove', (e) => touchEnd = e.targetTouches[0].clientX);
+nav.addEventListener("touchmove", (e) => (touchEnd = e.targetTouches[0].clientX));
 
-nav.addEventListener('touchend', () =>
-{
-    // Une différence (entre touchEnd et touchStart) d'au moins 100px est nécessaire pour que le menu se ferme
-    if (touchEnd - touchStart > 100) {
-        nav.classList.remove('active');
-        btn.classList.remove('active');
-        html.classList.remove('no-scroll');
-    }
+nav.addEventListener("touchend", () => {
+	// Une différence (entre touchEnd et touchStart) d'au moins 100px est nécessaire pour que le menu se ferme
+	if (touchEnd - touchStart > 100) {
+		nav.classList.remove("active");
+		btn.classList.remove("active");
+		html.classList.remove("no-scroll");
+	}
 });
 
 // Onglets
 let indexData = 0;
 
-ongletsLinks.forEach(link => 
-{
-    link.addEventListener('click', () =>
-    {
-        link.classList.add('active');
-        
-        indexData = link.dataset.anim;
-        console.log(indexData);
+ongletsLinks.forEach((link) => {
+	link.addEventListener("click", () => {
+		link.classList.add("active");
 
-        for (let i = 0; i < ongletsLinks.length; i++) 
-        {
-            if(ongletsLinks[i].dataset.anim != indexData)
-            {
-                ongletsLinks[i].classList.remove('active');
-            }
-        }
+		indexData = link.dataset.anim;
+		console.log(indexData);
 
-        for (let c = 0; c < contenu.length; c++) 
-        {
-            if(contenu[c].dataset.anim == indexData)
-            {
-                contenu[c].classList.add('activeContenu');
-            }
-            else
-            {
-                contenu[c].classList.remove('activeContenu');
-            }
-        }
-    }); 
+		for (let i = 0; i < ongletsLinks.length; i++) {
+			if (ongletsLinks[i].dataset.anim != indexData) {
+				ongletsLinks[i].classList.remove("active");
+			}
+		}
+
+		for (let c = 0; c < contenu.length; c++) {
+			if (contenu[c].dataset.anim == indexData) {
+				contenu[c].classList.add("activeContenu");
+			} else {
+				contenu[c].classList.remove("activeContenu");
+			}
+		}
+	});
 });
